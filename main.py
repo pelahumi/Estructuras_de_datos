e5525b7c8ae369f6c88bca1067af1b1fe3db7dd0
Pelayo <phuermij@myuax.com>
2022-04-17 16:23:58 +0200

    main

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from clases.iva import *
 if __name__ == "__main__":
     print("Ejercicio 3:")
     producto = FactoryFactura()
-    producto.set_naturaleza("alimentaria")
-    print(producto.iva())
+    producto.set_naturaleza("alimentación")
+    print("El precio del producto alimentario es:",producto.iva(),"€")
+    producto.set_naturaleza("servicio")
+    print("El precio del producto de servicio es:",producto.iva(),"€")
