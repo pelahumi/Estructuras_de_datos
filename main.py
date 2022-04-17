fdcbf592a0622be148fc8bb28869c08ae0a5aab7
Pelayo <phuermij@myuax.com>
2022-04-17 16:20:51 +0200

    cambios

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from clases.iva import *
 
 if __name__ == "__main__":
-    print("Ejercicio 3")
+    print("Ejercicio 3:")
+    producto = FactoryFactura()
+    producto.set_naturaleza("alimentaria")
+    print(producto.iva())
