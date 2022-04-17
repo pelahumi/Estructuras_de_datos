2eb48f3980d8ad4798325e6fa9a18e8cf3d459dc
Pelayo <phuermij@myuax.com>
2022-04-17 17:41:31 +0200

    main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from clases.visitante import * 
+from clases.filosofia import *
 from clases.iva import *
 
 if __name__ == "__main__":
+
     print("Ejercicio 1:")
     ok = Mostrar("Ok")
     ko = Mostrar("Ko")
@@ -9,6 +11,12 @@ if __name__ == "__main__":
     bloque = Bloque()
     bloque.agregarInstruction(condicion)
     mientras = MientrasQue(True, bloque)
+
+    print("Ejercicio 2:")
+    code1 = "Hola don Pepito"
+    code2 = "Hola don José"
+    escritura = FilosofiaMVC(code1, code2)
+    escritura.escribir_archivo()
 
     print("Ejercicio 3:")
     producto = FactoryFactura()
