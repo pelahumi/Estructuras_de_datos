ce4150845fea518948f91ceaf8f7a77bb414cf30
Pelayo <phuermij@myuax.com>
2022-04-17 14:55:31 +0200

    cambios

--- clases/iva.py
+++ clases/iva.py
@@ -1,5 +1,20 @@
 class Producto():
     def __init__(self, naturaleza):
-        self.precio = 100
+        self.precio_neto = 100
         self.naturaleza = naturaleza
+
+class FactoryFactura(Producto):
+    def __init__(self):
+        super.__init__()
+
+    def crear(self):
+        self.naturaleza = str(input("Introduce la naturaleza del producto: "))
+
+    def iva(self):
+        if self.naturaleza == "alimentación":
+            precio_bruto = self.precio_neto + (self.precio_neto * 5.5 / 100)
+            return precio_bruto
+        elif self.naturaleza == "servicio":
+            
+
 
